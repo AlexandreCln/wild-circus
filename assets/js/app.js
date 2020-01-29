4de122afe230319823703911c75882f44846492c
Alexandre Coulon <alex97.coul@gmail.com>
2020-01-29 14:10:06 +0100

    Webpack Encore, layout and navbar.

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@fortawesome/fontawesome-free/css/all.min.css');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@fortawesome/fontawesome-free/js/all.js');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@fortawesome/fontawesome-free/css/all.min.css');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@fortawesome/fontawesome-free/js/all.js');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('bootstrap');
+
+// any CSS you require will output into a single scss file (app.scss in this case)
+require('../scss/app.scss');
+
+// any CSS you import will output into a single scss file (app.scss in this case)
+import '../scss/app.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
